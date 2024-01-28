9964d8a21e524ebee01e46e3b65e0e306694faa8
Angela Li <angela.tylee25@gmail.com>
2024-01-28 22:21:17 +0800

    week2 commit

---
+++ script/log-in.js
@@ -0,0 +1,31 @@
+import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.4.15/vue.esm-browser.min.js";
+
+
+createApp ({
+  data() {
+    return {
+      // 使用者輸入的帳號(username)密碼(password)
+      user : {
+        username: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    login() {
+      const api = 'https://ec-course-api.hexschool.io/v2/admin/signin';
+      axios.post(api, this.user)
+        .then((response) => {
+          const { token, expired } = response.data;
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
+
+          // 登入成功後導向 products.html
+          window.location = 'products.html';
+        })
+        .catch((error) => {
+          console.log(error);
+          alert(error.data.message); 
+        })
+    }
+  }
+}).mount('#app');
